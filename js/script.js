0e97873ed8cb73387cb5d226f3b2e5727cf858b0
TFA05 <b2sinadinovic@gmail.com>
2025-04-16 00:02:23 +0200

    Add playRound function
    The functions determines who won the round and increases their score

--- js/script.js
+++ js/script.js
@@ -13,3 +13,50 @@ function getComputerChoice(){
 function getHumanChoice(){
     return prompt('Select between Rock, Paper and Scissors')
 }
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice){
+        return "Draw";
+    }
+    
+    if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            alert("You lose! Paper beats Rock")
+            humanScore++;
+        }
+        else{
+            alert("You win! Rock beats Scisors")
+            computerScore++;
+        }
+    }
+    else if (humanChoice === "paper"){
+        if (computerChoice === "scissors")
+            {
+                alert("You lose! Scissors beat Paper")
+                humanScore++;            
+        }
+        else{
+            alert("You win! Paper beats rock")
+            computerScore++;
+        }
+    }
+    else{
+        if (computerChoice === "rock"){
+            alert("You lose! Rock beats Scissors")
+            humanScore++;
+        }
+        else{
+            alert("You win! Scissors beat Paper")
+            computerScore++;
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
+
