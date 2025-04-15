f69ce4b4f3111978ffa643b0894fa13dde4ee5a3
TFA05 <b2sinadinovic@gmail.com>
2025-04-15 19:52:11 +0200

    Add function for generating a random move for computer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('bbb')
+function getComputerChoice(){
+    let randNumb = Math.floor(Math.random() * 3) + 1
+    switch (randNumb){
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        default:
+            return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
